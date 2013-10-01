d58c619a8cfd9b26096b1c0ff8e887077a4f89b0
Blanca Mora <ankisscyrus@gmail.com>
2013-10-01 16:23:14 -0500

    elimine carpetas duplicadas

---
+++ archivoPractica.php
@@ -0,0 +1,23 @@
+<?php
+
+//creando mi conexion
+$con = new mysqli('server','usuario','contraseña','bd');
+
+//var_dump($con);
+
+if ($con->connect_error){
+//echo "no me puedo conectar";
+	//exit();
+	die('No me puedo conectar');
+
+}
+
+$miquery = "INSERT INTO alumnos(nombre, codigo") VALUES 
+
+
+var_dump($resultado);
+
+
+
+?>
+
---
+++ js/validaciones.js
@@ -0,0 +1,24 @@
+function validaformulario(){
+
+//var regex = /^\d{5}$/
+//var form = document.getElementById('codigo');
+//var codigo = document.getElementById('codigo');
+
+var boton = document.getElementById('enviado');
+
+
+//if ()
+// 
+/*
+if(codigo.value==regex){
+	var div = document.createElement('div');
+	div.setAttribute('id','status_error');
+
+	var msg = document.createTextNode('Debes escribir algo');
+	div.appendChild(msg);
+*/
+
+
+
+
+}
