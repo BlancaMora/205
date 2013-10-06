9fc36d918d97b649999429bd1f3f0251617ce5c4
Blanca Mora <ankisscyrus@gmail.com>
2013-10-06 16:01:45 -0500

    validaciones admin

--- js/validaciones.js
+++ js/validaciones.js
@@ -14,7 +14,7 @@ if ( codigo != parseInt(codigo)){
 return;
 }
 
-if (codigo == "206724639" && password== "admin"){
+if (codigo == "1111" && password== "admin"){
 
 	window.open("administrador.html");
 }
@@ -31,4 +31,67 @@ if (codigo == "5678" && password== "alumno"){
 document.getElementById(login).submit();
 
 }
+
+//--------------Formulario usuariosAdministrador busca codigo
+
+function eliminado(){
+
+alert("Eliminado Correctamente");
+
+}
+
+
+function validaformulario(){
+	var codigo_buscar = document.getElementById("codigo_buscar").value;
+	
+if (codigo_buscar == ""){
+	alert("no se permiten datos vacios");
+return;
+}
+
+if (codigo_buscar != parseInt(codigo_buscar)){
+	alert("codigo debe ser un valor numerico");
+return;
+}
+
+document.getElementById(Buscar_por_codigo).submit();
+
+}
+
+function agregarNuevoUsuario(){
+	//alert("No se permite Agregar con Datos Vacios");
+
+var codigo_agregar = document.getElementById("agregar_codigo").value;
+var password_agregar = document.getElementById("agregar_password").value;
+
+if (codigo_agregar == "" || password_agregar == ""){
+	alert("No se permite Agregar con Datos Vacios");
+	return;
+}
+
+if (codigo_agregar != parseInt(codigo_agregar)){
+
+	alert("Ingresar un codigo numerico");
+	return;
+}
+
+if ( password_agregar.length < 4 ){
+		alert("Ingrese un password con mas caracteres");
+return;
+}
+document.getElementById(admin_agregarUsuario).submit();
+
+}
+
+function agregarFestivoALista(){
+
+	alert("dia festivo agregado");
+}
+
+function agregarCiclo(){
+
+alert("Ciclo Agregado");
+}
+
+
 
